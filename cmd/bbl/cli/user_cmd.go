1ddfab108d8952bf6d2daab1161a5cddc5c29c33
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2025-04-25 11:55:51 +0200

    less files

--- cmd/bbl/cli/user_cmd.go
+++ cmd/bbl/cli/user_cmd.go
@@ -10,6 +10,7 @@ import (
 
 func init() {
 	rootCmd.AddCommand(userCmd)
+	rootCmd.AddCommand(usersCmd)
 }
 
 var userCmd = &cobra.Command{
@@ -35,5 +36,33 @@ var userCmd = &cobra.Command{
 
 		enc := json.NewEncoder(cmd.OutOrStdout())
 		return enc.Encode(rec)
+	},
+}
+
+var usersCmd = &cobra.Command{
+	Use:   "users",
+	Short: "Users",
+	Args:  cobra.NoArgs,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		conn, err := pgxpool.New(cmd.Context(), config.PgConn)
+		if err != nil {
+			return err
+		}
+		defer conn.Close()
+
+		repo, err := pgxrepo.New(cmd.Context(), conn)
+		if err != nil {
+			return err
+		}
+
+		enc := json.NewEncoder(cmd.OutOrStdout())
+
+		for rec := range repo.UsersIter(cmd.Context(), &err) {
+			if err = enc.Encode(rec); err != nil {
+				return err
+			}
+		}
+
+		return err
 	},
 }
--- cmd/bbl/cli/work_cmd.go
+++ cmd/bbl/cli/work_cmd.go
@@ -5,11 +5,19 @@ import (
 
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/spf13/cobra"
+	"github.com/ugent-library/bbl/jobs"
 	"github.com/ugent-library/bbl/pgxrepo"
 )
 
 func init() {
 	rootCmd.AddCommand(workCmd)
+	rootCmd.AddCommand(worksCmd)
+	worksCmd.AddCommand(searchWorksCmd)
+	searchWorksCmd.Flags().StringVarP(&searchOpts.Query, "query", "q", "", "")
+	searchWorksCmd.Flags().IntVar(&searchOpts.Size, "size", 20, "")
+	searchWorksCmd.Flags().IntVar(&searchOpts.From, "from", 0, "")
+	searchWorksCmd.Flags().StringVar(&searchOpts.Cursor, "cursor", "", "")
+	worksCmd.AddCommand(reindexWorksCmd)
 }
 
 var workCmd = &cobra.Command{
@@ -35,5 +43,94 @@ var workCmd = &cobra.Command{
 
 		enc := json.NewEncoder(cmd.OutOrStdout())
 		return enc.Encode(rec)
+	},
+}
+
+var worksCmd = &cobra.Command{
+	Use:   "works",
+	Short: "Works",
+	Args:  cobra.NoArgs,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		conn, err := pgxpool.New(cmd.Context(), config.PgConn)
+		if err != nil {
+			return err
+		}
+		defer conn.Close()
+
+		repo, err := pgxrepo.New(cmd.Context(), conn)
+		if err != nil {
+			return err
+		}
+
+		enc := json.NewEncoder(cmd.OutOrStdout())
+
+		for rec := range repo.WorksIter(cmd.Context(), &err) {
+			if err = enc.Encode(rec); err != nil {
+				return err
+			}
+		}
+
+		return err
+	},
+}
+
+var reindexWorksCmd = &cobra.Command{
+	Use:   "reindex",
+	Short: "Start reindex works job",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		logger := newLogger(cmd.OutOrStdout())
+
+		conn, err := pgxpool.New(cmd.Context(), config.PgConn)
+		if err != nil {
+			return err
+		}
+		defer conn.Close()
+
+		repo, err := pgxrepo.New(cmd.Context(), conn)
+		if err != nil {
+			return err
+		}
+
+		index, err := newIndex(cmd.Context())
+		if err != nil {
+			return err
+		}
+
+		riverClient, err := newRiverClient(logger, conn, repo, index)
+		if err != nil {
+			return err
+		}
+
+		res, err := riverClient.Insert(cmd.Context(), jobs.ReindexWorks{}, nil)
+		if err != nil {
+			return err
+		}
+
+		if res.UniqueSkippedAsDuplicate {
+			logger.Info("works reindexer is already running")
+		} else {
+			logger.Info("started works reindexer", "job", res.Job.ID)
+		}
+
+		return nil
+	},
+}
+
+var searchWorksCmd = &cobra.Command{
+	Use:   "search",
+	Short: "Search works",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		index, err := newIndex(cmd.Context())
+		if err != nil {
+			return err
+		}
+
+		hits, err := index.Works().Search(cmd.Context(), searchOpts)
+		if err != nil {
+			return err
+		}
+
+		enc := json.NewEncoder(cmd.OutOrStdout())
+		return enc.Encode(hits)
 	},
 }
