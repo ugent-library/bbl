2b474c1e3620039f3a1d013168819dc83ee36f0f
Nicolas Steenlant <nicolas.steenlant@ugent.be>
2025-11-07 16:51:59 +0100

    comment completionIndex

--- opensearchindex/completion_index.go
+++ opensearchindex/completion_index.go
@@ -1,27 +1,20 @@
 package opensearchindex
 
 import (
-	"context"
 	_ "embed"
-	"encoding/json"
-	"strings"
-
-	"github.com/opensearch-project/opensearch-go/v4/opensearchapi"
-	"github.com/ugent-library/bbl"
-	"github.com/ugent-library/bbl/opensearchswitcher"
 )
 
 //go:embed completion_settings.json
-var completionSettings string
+// var completionSettings string
 
-type completionDoc struct {
-	Completion string `json:"completion"`
-}
+// type completionDoc struct {
+// 	Completion string `json:"completion"`
+// }
 
-type completionIndex struct {
-	client *opensearchapi.Client
-	alias  string
-}
+// type completionIndex struct {
+// 	client *opensearchapi.Client
+// 	alias  string
+// }
 
 // func newCompletionIndex(
 // 	ctx context.Context,
@@ -38,63 +31,63 @@ type completionIndex struct {
 // 	}, nil
 // }
 
-func (idx *completionIndex) NewSwitcher(ctx context.Context) (bbl.IndexSwitcher[string], error) {
-	return opensearchswitcher.New(ctx, opensearchswitcher.Config[string]{
-		Client:        idx.client,
-		Alias:         idx.alias,
-		IndexSettings: strings.NewReader(completionSettings),
-		Retention:     1,
-		ToItem: func(completion string) opensearchswitcher.Item {
-			return opensearchswitcher.Item{
-				Doc: completionDoc{completion},
-			}
-		},
-	})
-}
+// func (idx *completionIndex) NewSwitcher(ctx context.Context) (bbl.IndexSwitcher[string], error) {
+// 	return opensearchswitcher.New(ctx, opensearchswitcher.Config[string]{
+// 		Client:        idx.client,
+// 		Alias:         idx.alias,
+// 		IndexSettings: strings.NewReader(completionSettings),
+// 		Retention:     1,
+// 		ToItem: func(completion string) opensearchswitcher.Item {
+// 			return opensearchswitcher.Item{
+// 				Doc: completionDoc{completion},
+// 			}
+// 		},
+// 	})
+// }
 
-func (idx *completionIndex) Search(ctx context.Context, q string) (*bbl.CompletionHits, error) {
-	jQ, err := jsonString(q)
-	if err != nil {
-		return nil, err
-	}
+// func (idx *completionIndex) Search(ctx context.Context, q string) (*bbl.CompletionHits, error) {
+// 	jQ, err := jsonString(q)
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	body := `{
-		"query": {
-			"match_phrase_prefix": {
-				"completion": "` + jQ + `"
-			}
-		},
-		"size": 10,
-		"highlight": {
-    		"fields": {
-      			"completion": {
-					"type": "unified"
-				}
-    		}
-  		}
-	}`
+// 	body := `{
+// 		"query": {
+// 			"match_phrase_prefix": {
+// 				"completion": "` + jQ + `"
+// 			}
+// 		},
+// 		"size": 10,
+// 		"highlight": {
+//     		"fields": {
+//       			"completion": {
+// 					"type": "unified"
+// 				}
+//     		}
+//   		}
+// 	}`
 
-	res, err := idx.client.Search(ctx, &opensearchapi.SearchReq{
-		Indices: []string{idx.alias},
-		Body:    strings.NewReader(body),
-	})
-	if err != nil {
-		return nil, err
-	}
+// 	res, err := idx.client.Search(ctx, &opensearchapi.SearchReq{
+// 		Indices: []string{idx.alias},
+// 		Body:    strings.NewReader(body),
+// 	})
+// 	if err != nil {
+// 		return nil, err
+// 	}
 
-	hits := &bbl.CompletionHits{
-		Hits: make([]bbl.CompletionHit, len(res.Hits.Hits)),
-	}
-	for i, hit := range res.Hits.Hits {
-		var h bbl.CompletionHit
-		if err := json.Unmarshal(hit.Source, &h); err != nil {
-			return nil, err
-		}
-		if hl := hit.Highlight["completion"]; len(hl) > 0 {
-			h.Highlight.Completion = hl[0]
-		}
-		hits.Hits[i] = h
-	}
+// 	hits := &bbl.CompletionHits{
+// 		Hits: make([]bbl.CompletionHit, len(res.Hits.Hits)),
+// 	}
+// 	for i, hit := range res.Hits.Hits {
+// 		var h bbl.CompletionHit
+// 		if err := json.Unmarshal(hit.Source, &h); err != nil {
+// 			return nil, err
+// 		}
+// 		if hl := hit.Highlight["completion"]; len(hl) > 0 {
+// 			h.Highlight.Completion = hl[0]
+// 		}
+// 		hits.Hits[i] = h
+// 	}
 
-	return hits, nil
-}
+// 	return hits, nil
+// }
